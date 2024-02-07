bba96ca099c68f7f848ab621648b234304169e94
AndreBonnekoh <60172781+AndreBonnekoh@users.noreply.github.com>
2024-02-07 21:02:35 +0100

    implemented phoenix assets

---
+++ MazeGame/Assets/Scripts/PhoenixController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoenixController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+
+    private Vector2 input;
+
+    private Animator animator;
+
+    private Coroutine moveCoroutine;
+
+    /*private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        if (input != Vector2.zero)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                moveSpeed = 4;
+            }
+            else
+            {
+                moveSpeed = 2;
+            }
+
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
+
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+
+            var targetPosition = transform.position + new Vector3(input.x, input.y, 0f);
+            moveCoroutine = StartCoroutine(Move(targetPosition));
+        }
+        else if (isMoving)
+        {
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            isMoving = false;
+        }
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }*/
+
+
+}
